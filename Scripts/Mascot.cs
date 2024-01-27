b155199adf96c1376bffee3f00fced0d255eebd7
Jonathan Moriarty <jonathan.taylor.moriarty@gmail.com>
2024-01-27 15:15:15 -0500

    Added Jonathan work scene with placeholder mascot and script to make mascot do things.

---
+++ Scripts/Mascot.cs
@@ -0,0 +1,78 @@
+using Godot;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public partial class Mascot : Node2D {
+	private AnimatedSprite2D _animatedSprite2D;
+	private NinePatchRect _dialogBox;
+	private Label _dialogText;
+	
+	[Export]
+	private float FollowSpeed = 4.0f;
+	
+	private Vector2 TweenTarget;
+
+	public override void _Ready() {
+		_animatedSprite2D = GetNode<AnimatedSprite2D>("Sprite");
+		_dialogBox = GetNode<NinePatchRect>("DialogBox");
+		_dialogText = GetNode<Label>("DialogBox/DialogText");
+		TweenTarget = Position;
+	}
+
+	public override void _Input(InputEvent @event) {
+		//Just Debug to test stuff
+		if (@event is InputEventKey keyEvent && keyEvent.Pressed) {
+			if (keyEvent.Keycode == Key.W) {
+				GD.Print("W was pressed");
+				PlayAnimation("idle", 0f, 5f);
+			}
+			if (keyEvent.Keycode == Key.S) {
+				GD.Print("S was pressed");
+				Vector2 mousePos = GetGlobalMousePosition();
+				SetTweenTarget(mousePos, 2f);
+			}
+			if (keyEvent.Keycode == Key.E) {
+				PlayText("TEST text goes here stupid", 5f);
+			}
+		}
+	}
+
+	public override void _PhysicsProcess(double delta) {
+		Position = Position.Lerp(TweenTarget, (float)delta * FollowSpeed);
+	}
+
+	public override void _Process(double delta) {
+	}
+	
+	// Play Animation
+	public async Task PlayAnimation(string animationName, float startDelaySeconds = -1f, float stopAfterSeconds = -1f) {
+		if (startDelaySeconds > 0) {
+			int startDelayMS = (int)(startDelaySeconds * 1000);
+			await Task.Delay(startDelayMS);
+		}
+		_animatedSprite2D.Play(animationName);
+		if (stopAfterSeconds > 0f) {
+			int stopDelayMS = (int)(stopAfterSeconds * 1000);
+			await Task.Delay(stopDelayMS);
+			_animatedSprite2D.Stop();
+		}
+	}
+	
+	// Tween to location
+	public void SetTweenTarget(Vector2 targetPosition, float tweenTimeSeconds) {
+		TweenTarget = targetPosition;
+	}
+	
+	// Play Text
+	// -Needs to resize the text box to correct size before making the characters gradually visible
+	// -Tween textbox size or alpha before text starts
+	public async Task PlayText(string text, float textTimeSeconds) {
+		_dialogText.VisibleCharacters = 0;
+		_dialogText.Text = text;
+		int delayTimeMS = (int)(textTimeSeconds * 1000) / text.Length;
+		for (int i = 1; i <= text.Length; i++) {
+			_dialogText.VisibleCharacters = i;
+			await Task.Delay(delayTimeMS);
+		}
+	}
+}
