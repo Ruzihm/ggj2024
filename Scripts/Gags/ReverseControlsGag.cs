ae64d5d5fb39c1f2675359925230b1bd38c2d679
Jonathan Moriarty <jonathan.taylor.moriarty@gmail.com>
2024-01-28 12:20:23 -0500

    more fixes

--- Scripts/Gags/ReverseControlsGag.cs
+++ Scripts/Gags/ReverseControlsGag.cs
@@ -7,9 +7,10 @@ public partial class ReverseControlsGag : BaseGag
 	public Timer timer ;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-    {
-        _ = mascot.PlayText("Let's make sure you aren't working under the influence. Engaging sobriety test mode.", 3, 5);
-        cursor.ReverseControls();
+	{
+		//"Let's make sure you aren't working under the influence. Engaging sobriety test mode."
+		_ = mascot.PlayText("I noticed you're not going as fast as you could be. Let me turn on inverted controls for you, did that help?", 3, 5);
+		cursor.ReverseControls();
 	}
 
 	private void _on_timer_timeout()
